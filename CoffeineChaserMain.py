cd796054700e768bd045a25e4115083cbdeb291a
VemonCode <60397549+VemonCode@users.noreply.github.com>
2020-02-08 21:11:48 +0100

    Update CoffeineChaserMain.py

--- CoffeineChaserMain.py
+++ CoffeineChaserMain.py
@@ -1,1 +1,6 @@
-# Currently working on the code, will add it here when parts are done.
+import requests
+import warnings
+
+# I want to fucking die
+
+shittyRequest = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=cafe&location=LOCATION_LATITUDE,LOCATION_LONGITUDE&radius=2500&key=YOUR_API_KEY")
