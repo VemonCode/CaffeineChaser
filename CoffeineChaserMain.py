990a2a8183138e3675407f2e6a73716396b48783
VemonCode <60397549+VemonCode@users.noreply.github.com>
2020-02-14 22:01:52 +0100

    Update CoffeineChaserMain.py

--- CoffeineChaserMain.py
+++ CoffeineChaserMain.py
@@ -1,7 +1,10 @@
 import requests
-import warnings
+import json
 
 # I want to fucking die
 
-shittyRequest = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=cafe&location=LOCATION_LATITUDE,LOCATION_LONGITUDE&radius=2500&key=YOUR_API_KEY")
-print(shittyRequest)
+shittyRequest = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=cafe&location=LOCATION_LATITUDE,LOCATION_LONGTITUDE&radius=2500&key=YOUR_API_KEY")
+
+data = shittyRequest.json()["results"]
+for result in data:
+  print(result["name"] + result["location"] + result["opening_hours"] + result["open_now"])
